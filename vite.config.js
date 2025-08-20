a2c7c29600e8c8055371839643feb42f163eed3e
Kurumurthy@2005 <marennagarikurumurthy2005@gmail.com>
2025-08-20 18:38:36 +0530

    added vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // important for Vercel to serve from root
+  server: {
+    port: 5173, // local dev
+    open: true,
+  },
+  build: {
+    outDir: "dist", // Vercel expects build output here
+  },
+});
